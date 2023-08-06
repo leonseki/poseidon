6a44dc23cb903c8a0455bf71d0e74abfcf126760
leon <leon.seki.tokyo@gmail.com>
2023-08-06 20:44:07 +0900

    first commit

---
+++ src/test/java/cn/tokyo/st/poseidon/PoseidonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tokyo.st.poseidon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
